1b4500c903c8644f79f85bff55a55109f23923fc
Mikhaylov Gleb <geba_m@mail.ru>
2022-12-30 23:15:51 +0700

    Databases

--- data/all_models.py
+++ data/all_models.py
@@ -4,6 +4,7 @@ db = SqliteDatabase('data/database.db')
 
 
 class Doctor(Model):
+    id = PrimaryKeyField(unique=True)
     last_name = AnyField(null=False)
     first_name = AnyField(null=False)
     middle_name = AnyField(null=True)
@@ -15,6 +16,7 @@ class Doctor(Model):
 
 
 class Admin(Model):
+    id = PrimaryKeyField(unique=True)
     last_name = AnyField(null=False)
     first_name = AnyField(null=False)
     middle_name = AnyField(null=True)
@@ -35,6 +37,7 @@ class LoginData(Model):
 
 
 class Patient(Model):
+    id = PrimaryKeyField(unique=True)
     last_name = AnyField(null=False)
     first_name = AnyField(null=False)
     middle_name = AnyField(null=True)
@@ -47,7 +50,48 @@ class Patient(Model):
         database = db
 
 
+class Price(Model):
+    id = PrimaryKeyField(unique=True)
+    article = AnyField(null=False)
+    price = IntegerField(null=False)
+
+    class Meta:
+        database = db
+
+
+class Preparations(Model):
+    id = PrimaryKeyField(unique=True)
+    article = AnyField(null=False)
+    name = AnyField(null=False)
+
+    class Meta:
+        database = db
+
+
+class Services(Model):
+    id = PrimaryKeyField(unique=True)
+    article = AnyField(null=False)
+    name = AnyField(null=False)
+
+    class Meta:
+        database = db
+
+
+class Note(Model):
+    id = PrimaryKeyField(unique=True)
+    Patient_id = AnyField(null=False)
+    Doctor_id = AnyField(null=False)
+    date = DateTimeField(null=False)
+
+    class Meta:
+        database = db
+
+
 Doctor.create_table()
 LoginData.create_table()
 Admin.create_table()
 Patient.create_table()
+Price.create_table()
+Preparations.create_table()
+Services.create_table()
+Note.create_table()
